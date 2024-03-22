725641f36f7b3ea1339d7dc7ce3c8461224cc950
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-03-22 18:57:53 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,14 +1,5 @@
-# Install an especific version of flask 2.1.0.
-
-class { 'python' :
-  version    => '3',
-  pip        => 'present',
-  dev        => 'present',
-  virtualenv => 'present',
-}
-
-python::pip { 'Flask':
-  pkgname    => 'Flask',
-  ensure     => '2.1.0',
-  virtualenv => '/var/www/project1',
+#This code will install the package puppet-lint
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
