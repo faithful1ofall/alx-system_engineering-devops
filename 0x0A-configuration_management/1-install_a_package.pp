55688eebaf96a1b3a0514cd24f735116704ae619
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-03-22 18:24:24 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
-# Install an especific version of flask (2.1.0)
+# Install an especific version of flask 2.1.0.
 
-exec { 'install_flask':
-  command => 'pip3 install Flask',
-  unless  => 'pip3 freeze | grep Flask',
+exec { 'install_flask_2_1_0':
+  command => 'pip3 install Flask==2.1.0',
+  unless  => 'pip3 freeze | grep Flask==2.1.0',
   path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
 }
