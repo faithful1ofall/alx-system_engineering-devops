8b909198114e994cb7a97201ff1c2b607c1aa7ec
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-03-22 19:04:10 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/pup
-#This code will install the package puppet-lint
+# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
+  provider => 'pip3',
+}
+
+# Install Werkzeug 2.1.1
+package { 'werkzeug':
+  ensure   => '2.1.1',
   provider => 'pip3',
 }
