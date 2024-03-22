cca59f50549383defd28085873d00fab33baa87a
faithful1ofall <50761148+faithful1ofall@users.noreply.github.com>
2024-03-22 13:06:37 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,9 @@
 # Install an especific version of flask (2.1.0)
-class python_setup {
-  class { 'python':
-    version    => 'system',
-    pip        => 'present',
-    dev        => 'present',
-    virtualenv => 'present',
-  }
-}
 
 class flask_install {
   package { 'Flask':
     ensure   => '2.1.0',
-    provider => 'pip3',
-    require  => Class['python_setup'],
+    provider => 'pip3'
   }
 }
-
-include python_setup
 include flask_install
